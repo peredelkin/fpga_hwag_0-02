76f42022b585491acaed87b1a1e588aad72b3d1a
Ruslan <ep8kha@gmail.com>
2020-10-12 21:01:35 +0500

    counter

--- flipflop.sv
+++ flipflop.sv
@@ -4,22 +4,22 @@
 module d_flip_flop #(parameter WIDTH=1) (
 input wire clk,
 input wire ena,
+input wire sload,
+input wire [WIDTH-1:0] d_load,
 input wire [WIDTH-1:0] d,
 input wire srst,
 input wire arst,
 output reg [WIDTH-1:0] q );
 
 always @(posedge clk,posedge arst) begin
-	if(arst) begin
-		q <= 0;
-	end else begin
-		if(srst) begin
-			q <= 0;
-		end else begin
-			if(ena) begin
-				q <= d;
-			end
-		end
+				if(arst) begin
+					q <= 0;
+	end else if(srst) begin
+					q <= 0;
+	end else if(sload) begin
+					q <= d_load;
+	end else if(ena) begin
+					q <= d;
 	end
 end
 
